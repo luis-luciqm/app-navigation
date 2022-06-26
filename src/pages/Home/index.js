07c7e8cbc62a979b58c02056231c5dc047687c39
luis-luciqm <redbullfc18@gmail.com>
2022-06-26 11:54:17 -0300

    adicionando routes

---
+++ src/pages/Home/index.js
@@ -0,0 +1,8 @@
+import React from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function Home(){
+    <View>
+        <Text>Oi som</Text>
+    </View>
+}
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+export function Routes(){
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="Home" component={HomeScreen} />
+                <Tab.Screen name="Settings" component={SettingsScreen} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
