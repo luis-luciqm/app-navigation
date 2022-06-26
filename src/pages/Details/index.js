a9120fca37c3811be5003e36be59e76bf763667e
luis-luciqm <redbullfc18@gmail.com>
2022-06-26 18:33:13 -0300

    Adicionando informações sobre o curso superior

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -2,13 +2,26 @@ import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
 export function Details(props){
-    console.log('\n\n\n\nsou euuuuu\n\n', props.route.params.object_cursos)
+    console.log('\n\n\n\nsou euuuuu\n\n', props.route.params.name)
 
     return (
-        <Text>Eu sou os detalhes, aaaa</Text>
+        <View style={styles.container}>
+            <Text style={styles.text}>Seja muito bem vindo ao curso de {props.route.params.name}</Text>
+            <Text style={styles.text}>{props.route.params.description}</Text>
+            <Text style={styles.text}>{props.route.params.name} é um curso simplemente incrivel, matricule-se já</Text>
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        textAlign: 'center',
+        padding: 30
+    }
 });
--- src/pages/Superior/index.js
+++ src/pages/Superior/index.js
@@ -3,31 +3,40 @@ import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 
 export default function Superior({navigation}){
 
-    const object_cursos = {
-        name: "ADS"
-    };
-
-    // function navigationDetails(){
-    //     navigation.navigate('details', {curso: "sssj"})
-    // }
+    const object_cursos = [
+        {
+            name: 'Análise e Desenvolvimento de Sistemas',
+            description: 'Con nota máxima no ENADE, o curso de Análise e Desenvolvimento de Sistemas demonstra cada dia ser o melhor curso do IF',
+        },
+        {
+            name: 'Agroindustria',
+            description: 'Iniciando em 2022, o curso de Agroindustria tem a função de plantar plantas e só',
+        },
+        {
+            name: 'Quimica',
+            description: 'Quimica é foda, não recomendo',
+        },
+    ]
 
     return (
         <View style={styles.container}>
             <TouchableOpacity   
                 style={styles.button}
-                onPress={ () => navigation.navigate('details', {object_cursos})}
+                onPress={ () => navigation.navigate('details', object_cursos[0])}
             >
                 <Text>Análise e Desenvolvimento de Sistemas</Text>
             </TouchableOpacity>
 
             <TouchableOpacity
                 style={styles.button}
+                onPress={ () => navigation.navigate('details', object_cursos[1])}
             >
                 <Text>Agroindústria</Text>
             </TouchableOpacity>
 
             <TouchableOpacity
                 style={styles.button}
+                onPress={ () => navigation.navigate('details', object_cursos[2])}
             >
                 <Text>Química</Text>
             </TouchableOpacity>
