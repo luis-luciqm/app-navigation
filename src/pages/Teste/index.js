f0a62716b6f60a8ed3013cf91e9cdfbd9bfb716d
luis-luciqm <redbullfc18@gmail.com>
2022-06-26 11:59:41 -0300

    teste

---
+++ src/pages/Teste/index.js
@@ -0,0 +1,8 @@
+import React from 'react-native';
+import { StyleSheet, View, Text } from 'react-native-web';
+
+export default function Settings(){
+    <View>
+        <Text>Sou o Settings</Text>
+    </View>
+}
